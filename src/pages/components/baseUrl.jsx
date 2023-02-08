9115210743b74f99456711092b1e9d376d5c8fd5
Traders of Africa <nnaemeka@tradersofafrica.com>
2023-02-08 14:04:01 +0100

    added netlify.toml page

--- src/pages/components/baseUrl.jsx
+++ src/pages/components/baseUrl.jsx
@@ -3,3 +3,13 @@ import api from "axios"
 export const axios = api.create({
     withCredentials: true
 })
+
+// import axios from axios
+
+// const baseUrl = process.env.REACT_APP_BACKEND_URL
+
+// const axios = axios.create({
+//     baseUrl:baseUrl,
+// })
+
+// export default axios
